008a5b46eb243f5f929db02928320cbbd7e00100
hara7610s <hara7610s@gmail.com>
2022-04-03 23:52:17 +0900

    bug fix

--- checking_entry.py
+++ checking_entry.py
@@ -2,19 +2,15 @@ import time
 import line
 
 def notify_entry(entry_dict, my_dict):
+    notify_list = []
     for my_horse in my_dict:
         if my_horse in list(entry_dict.keys()):
             RaceCourse = entry_dict[my_horse][0].decode('utf-8')
             RaceNum = entry_dict[my_horse][1].decode('utf-8')
             RaceName = entry_dict[my_horse][2].decode('utf-8')
             HorseName = my_horse.decode('utf-8')
-            comments = my_dict[my_horse]
-            text = ''
-            for comment in comments:
-                if comment.decode('utf-8') != '':
-                    text = text + '\n' + comment.decode('utf-8')
-                else:
-                    break
             
-            line.notify_message(f'\n{RaceCourse}{RaceNum} {RaceName}\n{HorseName}{text}')
+            notify_list.append('\n{RaceCourse}{RaceNum} {RaceName} {HorseName}')
             time.sleep(1)
+    
+    line.notify_message(notify_list)
