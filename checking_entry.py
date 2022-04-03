7f8b2689a8dc40f9d2b9165f5d726f29b21b3131
hara7610s <hara7610s@gmail.com>
2022-04-03 23:09:26 +0900

    test commit

---
+++ checking_entry.py
@@ -0,0 +1,20 @@
+import time
+import line
+
+def notify_entry(entry_dict, my_dict):
+    for my_horse in my_dict:
+        if my_horse in list(entry_dict.keys()):
+            RaceCourse = entry_dict[my_horse][0].decode('utf-8')
+            RaceNum = entry_dict[my_horse][1].decode('utf-8')
+            RaceName = entry_dict[my_horse][2].decode('utf-8')
+            HorseName = my_horse.decode('utf-8')
+            comments = my_dict[my_horse]
+            text = ''
+            for comment in comments:
+                if comment.decode('utf-8') != '':
+                    text = text + '\n' + comment.decode('utf-8')
+                else:
+                    break
+            
+            line.notify_message(f'\n{RaceCourse}{RaceNum} {RaceName}\n{HorseName}{text}')
+            time.sleep(1)
---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+import line
+import find_race
+import find_horse
+import datetime
+import spreadsheet
+import checking_entry
+
+def main():
+    # find race pages tomorrow
+    tomorrow = datetime.date.today() + datetime.timedelta(days=7)
+    race_id_list = find_race.get_race_list(tomorrow)
+    
+    # make a list of horses entered tomorrow
+    if len(race_id_list) == 0:
+        pass
+    else:
+        entry_dict = {}
+        for race_id in race_id_list:
+            each_race = find_horse.make_entry_dict(race_id)
+
+            entry_dict.update(each_race)
+
+        # call my horse list
+        my_dict = spreadsheet.read_spreadsheet()
+
+        line.notify_message(tomorrow)
+        
+        # match entry list and my horse list
+        checking_entry.notify_entry(entry_dict, my_dict)
+
+if __name__ == "__main__":
+    main()
